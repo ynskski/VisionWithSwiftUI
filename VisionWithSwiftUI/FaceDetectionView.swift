003b8098081d2b829fd3e6d0d0c7815e5b5542cb
Yunosuke Sakai <qmkns246@gmail.com>
2020-10-28 14:22:05 +0900

    Viewのリネーム

--- VisionWithSwiftUI/FaceDetectionView.swift
+++ VisionWithSwiftUI/FaceDetectionView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  FaceDetectionView.swift
 //  VisionWithSwiftUI
 //
 //  Created by YunosukeSakai on 2020/10/28.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FaceDetectionView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        FaceDetectionView()
     }
 }
