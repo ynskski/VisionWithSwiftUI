3dc5072984710b6c14b960c4f0bbc84a9b078153
Yunosuke Sakai <qmkns246@gmail.com>
2020-10-28 14:18:22 +0900

    Initial Commit

---
+++ VisionWithSwiftUI/VisionWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionWithSwiftUIApp.swift
+//  VisionWithSwiftUI
+//
+//  Created by YunosukeSakai on 2020/10/28.
+//
+
+import SwiftUI
+
+@main
+struct VisionWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
